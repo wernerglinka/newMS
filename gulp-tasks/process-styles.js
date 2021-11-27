79b0ee05f400f48b6f6b17c558866d19626b046d
Werner Glinka <werner@glinka.co>
2021-11-26 20:32:47 -0600

    added conditionl ghulp for production build

--- gulp-tasks/process-styles.js
+++ gulp-tasks/process-styles.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+var gulpif = require('gulp-if');
 const util = require('gulp-util');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
@@ -9,6 +10,8 @@ const browserSync = require('browser-sync').create();
 
 const paths = require("./paths");
 
+const isProduction = !!util.env.production;
+
 module.exports = function styles() {
   console.log('Processing styles ************************');
   
@@ -18,11 +21,11 @@ module.exports = function styles() {
   return (
       gulp
           .src(paths.styles.src)
-          .pipe(sourcemaps.init())
+          .pipe(gulpif(!isProduction, sourcemaps.init()))
           .pipe(sass())
           .on("error", sass.logError)
           .pipe(postcss([autoprefixer(), cssnano()]))
-          .pipe(sourcemaps.write())
+          .pipe(gulpif(!isProduction, sourcemaps.write()))
           .pipe(gulp.dest(paths.styles.dest))
           // Add browsersync stream pipe after compilation
           .pipe(browserSync.stream())
