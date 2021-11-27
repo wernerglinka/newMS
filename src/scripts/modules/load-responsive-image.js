b049408e3eb9cff78f36b89714141cb4292bdc94
Werner Glinka <werner@glinka.co>
2021-11-26 19:17:49 -0600

    fixed some responsive image loading issues

--- src/scripts/modules/load-responsive-image.js
+++ src/scripts/modules/load-responsive-image.js
@@ -1,22 +1,52 @@
-function loadResponsiveImage() {
-  console.log("loadResponsiveImage");
+import debounce from "../utilities/debounce";
 
-  const allHiResImageWrappers = document.querySelectorAll(".js-progressive-image-wrapper");
+const loadImage = ((entries) => {
 
-  for ( let imageWrapper of allHiResImageWrappers ) {
-    const imageWidth = imageWrapper.clientWidth;
-    const imageHeight = imageWrapper.clientHeight;
-    const pixelRatio = window.devicePixelRatio || 1.0;
-    const imageParams = `w_${100 * Math.round((imageWidth * pixelRatio) / 100)},h_${100 * Math.round((imageHeight * pixelRatio) / 100)},c_fill,g_auto,f_auto`;
+  for (let entry of entries) {
+    if ( entry.isIntersecting) {
+      const thisWrapper = entry.target;
 
-    const thisImage = imageWrapper.querySelector(".high-res");
-    const thisImagePrefix = thisImage.dataset.prefix;
-    const thisImageSource = thisImage.dataset.source;
+      // get the dimensions of the image wrapper and the display pixel density
+      const imageWidth = thisWrapper.clientWidth;
+      const imageHeight = thisWrapper.clientHeight;
+      const pixelRatio = window.devicePixelRatio || 1.0;
+      
+      // assemble url parameters for the cloudinary image url
+      const imageParams = `w_${100 * Math.round((imageWidth * pixelRatio) / 100)},f_auto`;
 
-    thisImage.src = `${thisImagePrefix}${imageParams}/${thisImageSource}`;
+      // find the high res image in the wrapper and get the data attributes...
+      const thisImage = thisWrapper.querySelector(".high-res");
+      const thisImagePrefix = thisImage.dataset.prefix;
+      const thisImageSource = thisImage.dataset.source;
+      // ...so we can assemble and replace the image src url
+      thisImage.src = `${thisImagePrefix}${imageParams}/${thisImageSource}`;
+      // once the hi-res image has been loaded, add done class to wrapper
+      // which will fade-in the hi-res image
+      thisImage.onload = () => {
+        thisWrapper.classList.add("done");
+      };
+    }
   }
-  
+});
 
+
+const updateImage = debounce(function() {
+  // images are only loaded when they are visible
+  const observer = new IntersectionObserver(loadImage);
+
+  // loop over all image wrappers and add to intersection observer
+  const allHiResImageWrappers = document.querySelectorAll(".js-progressive-image-wrapper");
+  for ( let imageWrapper of allHiResImageWrappers ) {
+    observer.observe(imageWrapper);
+  }
+}, 500);
+
+
+function loadResponsiveImage() {
+  // images will update on page load and after a resize
+  const resizeObserver = new ResizeObserver(updateImage);
+  const resizeElement = document.body;
+  resizeObserver.observe(resizeElement);
 }
 
 export default loadResponsiveImage;
